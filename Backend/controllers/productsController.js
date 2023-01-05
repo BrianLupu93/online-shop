278f40fc35ca10d05a3a5b4beb5116a10edbc4d8
BrianLupu93 <brianlupu18@gmail.com>
2023-01-05 17:47:14 +0100

    addToCart route

--- Backend/controllers/productsController.js
+++ Backend/controllers/productsController.js
@@ -8,6 +8,10 @@ const path = require("path");
 const fsPromises = require("fs").promises;
 const { v4: uuid } = require("uuid");
 
+//**********************************************************
+//***************** ADD NEW PRODUCT ************************
+//**********************************************************
+
 const addNewProduct = async (req, res) => {
   const product = req.body;
 
@@ -22,7 +26,21 @@ const addNewProduct = async (req, res) => {
 
   const filteredObject = Object.fromEntries(filteredArray);
 
-  const currentProduct = { ...filteredObject, id: uuid() };
+  const reviews = Math.floor(Math.random() * 23);
+  const starsNr = Math.floor(Math.random() * (5 - 2) + 2);
+  const starsArray = [];
+  for (let i = 0; i <= starsNr; i++) {
+    starsArray.push(i);
+  }
+
+  const currentProduct = {
+    ...filteredObject,
+    id: uuid(),
+    reviews: reviews,
+    stars: starsArray,
+  };
+
+  console.log(currentProduct);
 
   try {
     productsDB.setProducts([...productsDB.products, currentProduct]);
@@ -38,10 +56,41 @@ const addNewProduct = async (req, res) => {
   }
 };
 
+//**********************************************************
+//***************** GET ALL PRODUCTS ***********************
+//**********************************************************
+
 const getAllProducts = (req, res) => {
   const products = productsDB.products;
 
   res.status(200).json(products);
 };
 
-module.exports = { addNewProduct, getAllProducts };
+//**********************************************************
+//********************** ADD TO CART ***********************
+//**********************************************************
+
+const addProductToCart = async (req, res) => {
+  const productId = req.params.id;
+
+  if (!productId) return res.status(401).json("Product id invalid or missing");
+
+  const foundProduct = productsDB.products.find(
+    (prod) => prod.id === productId
+  );
+
+  if (!foundProduct)
+    return res.status(401).json(`Product with id ${productId} not found`);
+
+  const arrayProduct = Object.entries(foundProduct);
+
+  const filteredProduct = arrayProduct.filter(
+    (row) => row[0] !== "stock" && row[0] !== "order"
+  );
+
+  const objectProduct = Object.fromEntries(filteredProduct);
+
+  res.status(201).json(objectProduct);
+};
+
+module.exports = { addNewProduct, getAllProducts, addProductToCart };
--- Backend/routes/api/products.js
+++ Backend/routes/api/products.js
@@ -4,5 +4,6 @@ const productsController = require("../../controllers/productsController");
 
 router.post("/add-new-product", productsController.addNewProduct);
 router.get("/get-products", productsController.getAllProducts);
+router.get("/add-to-cart/:id", productsController.addProductToCart);
 
 module.exports = router;
